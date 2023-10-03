ed4246a88e8200e5d7235fe4a6b3bcda6cb2f9a0
Rhiju Das <rhiju@stanford.edu>
2023-10-03 13:09:08 -0700

    compile_mae_table.m

---
+++ matlab/compile_mae_table.m
@@ -0,0 +1,32 @@
+function compile_mae_table( subs, sub_names, data, usages, scorefn );
+% compile_mae_table( subs, sub_names, data, usages, scorefn );
+%
+% Inputs
+%  subs = cell of tables of submissions
+%  sub_names = cell of strings of submission names
+%  data = table with solution data
+%  usages = cell of strings with usage tags to look for (e.g.,
+%    {'Private','Public}). Give [] to try all Usage values in data.
+%  scorefn = scoring function to use (default is @score_mae_standard)
+%
+%
+% (C) R. Das, Stanford University & HHMI, 2023
+if ~exist( 'usages', 'var' ) | isempty(usages); usages = setdiff(unique(data.Usage,'stable'),{'Ignored'}); end;
+if ~exist( 'scorefn','var') scorefn = @score_mae_standard; end;
+fprintf('\n');
+score = [];
+assert( length(subs) == length(sub_names) );
+for i = 1:length(subs)
+    for j = 1:length(usages)
+        score(i,j) = scorefn(subs{i}(1:100:end,:),data(1:100:end,:),usages{j});
+        fprintf('%s: %.5f  ',usages{j}, score(i,j));
+    end
+    fprintf(' %s\n',sub_names{i});
+end
+
+plot( score,'o-','linew', 2 );
+xlabel('Subset');
+ylabel('Score');
+legend( usages,'interpreter','none');
+set(gca,'xtick',[1:length(subs)],'xticklabel',sub_names,'ticklabelinterpreter','none','fontweight','bold')
+set(gcf, 'color','white');
